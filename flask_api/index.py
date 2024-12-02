ec3dc456a5c76d4132bdc12f565b5359b1e76740
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-02 20:49:09 +0700

    docs(flask): Add docs

--- flask_api/index.py
+++ flask_api/index.py
@@ -11,15 +11,25 @@ WINDOW_SIZE = 52  # 1 year
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
+    # Get stock and steps from request
     stock = str(data['stock']).upper()
     steps = int(data['steps'])
+
+    # Load model and scaler based on the stock
     model = tf.keras.models.load_model(f"../stock_model/{stock}/model_saham.h5")
     scaler = joblib.load(f"../stock_model/{stock}/scaler.pkl")
+
+    # Get the data
     TIME, SERIES = parse_data_from_file(f"../stock_model/{stock}/data_saham.csv")
     SERIES = scaler.fit_transform(SERIES.reshape(-1, 1)).flatten()
-    # return "MASUK"
+
+    # Predict
     predicted_values = extended_forecast(model, SERIES, WINDOW_SIZE, forecast_steps=steps)
+
+    # Convert to actual price
     predicted_actual = scaler.inverse_transform([predicted_values])
+
+    # Return json (array)
     return jsonify({
         'predictions': predicted_actual.flatten().tolist()
     })
